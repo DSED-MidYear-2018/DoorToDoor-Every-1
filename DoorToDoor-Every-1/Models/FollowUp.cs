a34a5a9988a98536a9a0478b6f9b283bc1e187c4
Danny Beckett <k1w1onoe@hotmail.com>
2018-04-04 13:45:06 +1200

    Followup Class

---
+++ DoorToDoor-Every-1/Models/FollowUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoorToDoor-Every-1.Models
+{
+    public class FollowUp
+    {
+    public string Id 
+
+    }
+}
