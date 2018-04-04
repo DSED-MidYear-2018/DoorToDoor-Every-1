2d1d068f877ef2b74068350f3191468507fa2ccd
Danny Beckett <k1w1onoe@hotmail.com>
2018-04-04 13:58:42 +1200

    ccurance - complete

---
+++ DoorToDoor-Every-1/Models/Occurance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoorToDoor-Every-1.Models
+{
+    public class Occurance
+    {
+    public string Id { get; set; }
+    public string Title { get; set; }
+    }
+}
