8288a8caab407d0c98180ee1665494858b5ac08f
Alisdair <Alisdair@LAPTOP-TKI3D179>
2018-04-04 14:26:38 +1200

    ’Twas brillig, and the slithy toves       Did gyre and gimble in the wabe

---
+++ DoorToDoor-Every-1/Models/Home.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class Home
+{
+
+public int Id { get; set; }
+
+public bool DoorAnswered { get; set; }
+
+public bool Interested { get; set; }
+
+public string Notes { get; set; }
+
+public DateTime Visited { get; set; }
+
+public bool FollowUp { get; set; }
+}
