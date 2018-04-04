b650e09e59138707e211e76e41e6a3dd34a851ac
Danny Beckett <k1w1onoe@hotmail.com>
2018-04-04 14:36:12 +1200

    TidyUp FollowUp and Occurnce

--- DoorToDoor-Every-1/Models/FollowUp.cs
+++ DoorToDoor-Every-1/Models/FollowUp.cs
@@ -3,15 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DoorToDoor-Every-1.Models
-{
+
     public class FollowUp
     {
-    public string Id { get; set; }
+    public int Id { get; set; }
     public string DateReturn { get; set;  }
     public string TimeReturn { get; set;  }
     public string HomeId_F { get; set; }
-    public string OccurenceID_F { get; set; }
-
+    public Occurance OccurenceID_F { get; set; }
     }
-}
+
--- DoorToDoor-Every-1/Models/Occurance.cs
+++ DoorToDoor-Every-1/Models/Occurance.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DoorToDoor-Every-1.Models
-{
+
     public class Occurance
     {
     public string Id { get; set; }
     public string Title { get; set; }
     }
-}
+
