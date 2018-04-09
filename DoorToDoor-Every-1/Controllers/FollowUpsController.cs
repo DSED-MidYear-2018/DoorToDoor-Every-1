58752a63bc72311d1aacacf355df3b56da484a60
Alisdair <Alisdair@LAPTOP-TKI3D179>
2018-04-09 12:15:44 +1200

    Yam soup

---
+++ DoorToDoor-Every-1/Controllers/FollowUpsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DoorToDoor_Every_1.Data;
+using DoorToDoor_Every_1.Models;
+
+namespace DoorToDoor_Every_1.Controllers
+{
+    public class FollowUpsController : Controller
+    {
+        private readonly DoorContext _context;
+
+        public FollowUpsController(DoorContext context)
+        {
+            _context = context;
+        }
+
+        // GET: FollowUps
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.FollowUps.ToListAsync());
+        }
+
+        // GET: FollowUps/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var followUp = await _context.FollowUps
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (followUp == null)
+            {
+                return NotFound();
+            }
+
+            return View(followUp);
+        }
+
+        // GET: FollowUps/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FollowUps/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,DateReturn,TimeReturn")] FollowUp followUp)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(followUp);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(followUp);
+        }
+
+        // GET: FollowUps/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var followUp = await _context.FollowUps.SingleOrDefaultAsync(m => m.Id == id);
+            if (followUp == null)
+            {
+                return NotFound();
+            }
+            return View(followUp);
+        }
+
+        // POST: FollowUps/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,DateReturn,TimeReturn")] FollowUp followUp)
+        {
+            if (id != followUp.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(followUp);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FollowUpExists(followUp.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(followUp);
+        }
+
+        // GET: FollowUps/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var followUp = await _context.FollowUps
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (followUp == null)
+            {
+                return NotFound();
+            }
+
+            return View(followUp);
+        }
+
+        // POST: FollowUps/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var followUp = await _context.FollowUps.SingleOrDefaultAsync(m => m.Id == id);
+            _context.FollowUps.Remove(followUp);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FollowUpExists(int id)
+        {
+            return _context.FollowUps.Any(e => e.Id == id);
+        }
+    }
+}
