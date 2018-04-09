7cdf881eb2841d0f0ce81ae859aeb2e97606fab3
amandahyde <amandahyde84@gmail.com>
2018-04-09 14:54:49 +1200

    login/register work in progress

--- DoorToDoor-Every-1/Models/Admin.cs
+++ DoorToDoor-Every-1/Models/Admin.cs
@@ -12,13 +12,29 @@ namespace DoorToDoor_Every_1.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+
+       
 
         [Display(Name = "Remember me?")]
         public bool RememberMe { get; set; }
 
         public List<AdminRole> AdminRoles { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
     }
 }
 
