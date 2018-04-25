15c89da27857b03524e4e0e269a1c54da68fb16c
amandahyde <amandahyde84@gmail.com>
2018-04-26 11:41:10 +1200

    trying to work out user roles

--- DoorToDoor-Every-1/Models/AccountViewModels/RegisterViewModel.cs
+++ DoorToDoor-Every-1/Models/AccountViewModels/RegisterViewModel.cs
@@ -23,5 +23,14 @@ namespace DoorToDoor_Every_1.Models.AccountViewModels
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        public int Role { get; set; }
+
+
+
+        
     }
+
+
+
 }
