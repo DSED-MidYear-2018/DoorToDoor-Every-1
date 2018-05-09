b88aca6e398923d215f531b2a732c45702862bba
amandahyde <amandahyde84@gmail.com>
2018-05-09 12:00:08 +1200

    Done

--- DoorToDoor-Every-1/Models/AccountViewModels/RegisterViewModel.cs
+++ DoorToDoor-Every-1/Models/AccountViewModels/RegisterViewModel.cs
@@ -13,6 +13,10 @@ namespace DoorToDoor_Every_1.Models.AccountViewModels
         [Display(Name = "Email")]
         public string Email { get; set; }
 
+
+        public int Id { get; set; }
+
+
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
